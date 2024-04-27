f60fa771389f46b5580ccbc5a085cfa244b938a6
vt <vt>
2024-04-27 15:33:51 +0000

    added APItest.js

---
+++ APItest.js
@@ -0,0 +1,31 @@
+﻿const { TwitterApi } =require('twitter-api-v2');
+const moment = require('moment-timezone');
+const conf = require('config');
+
+const agents=[
+	{
+		itemName: 'キャシーイヤリング',
+		bot: new TwitterApi(conf.bot1)
+	},
+	{
+		itemName: 'ブリッツリング',
+		bot: new TwitterApi(conf.bot2)
+	},
+	{
+		itemName: 'スコルの牙',
+		bot: new TwitterApi(conf.bot3)
+	}
+];
+
+async function postTweet(agent, str) {
+	try {
+		ret= await agent.bot.v2.tweet(str);
+        console.log("success!: "+ret.data.text);
+	} catch (e) {
+        console.error(e)
+    }
+}
+
+for( var i=0; i<agents.length; i++ ) {
+    postTweet(agents[i], "【テスト】"+agents[i].itemName);
+}
